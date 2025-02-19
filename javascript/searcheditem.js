a311353619b45f20efcce5bc1a3f087bfbab0947
Shroukmahmoud240 <shroukmahmoud140@gmail.com>
2025-02-19 15:14:12 +0200

    Add files via upload

---
+++ javascript/searcheditem.js
@@ -0,0 +1,28 @@
+// let parent =document.querySelector(".parent")
+// let item=localStorage.getItem("searched")
+// if(item){
+//     let newitem=JSON.parse(item)
+//     draw(newitem);
+// }
+// function draw(product){
+//     let y=  `
+//     <div class="item ">
+//   <div class="image">
+//       <img src="${product.imageUrl1}" class="d-block w-100  " id="img" alt="...">
+//   </div><!-- /image -->
+//   <div class="item_info">
+//     <h4>${product.title}</h4>
+//     <p>Lorem ipsum dolor sit amet consectetur.</p>
+//     <div class="price_div">
+// <b><p>price :</p></b>
+//     <div class="price"> ${product.price}</div>
+//     </div><!-- /price_div -->
+// </div><!-- /item_info -->
+// <div class="fav">
+// <button class="addcart" onclick="check(${product.id})">remove</button>
+// </div>
+// </div>
+// `
+// parent.innerHeight=y
+
+// }
