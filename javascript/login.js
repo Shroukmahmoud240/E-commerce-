9f97f08f6839f65c2540862dff83b4c1a4370a67
Shrouk Mahmoud <shroukmahmoud140@gmail.com>
2026-05-06 15:01:37 +0300

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,24 +1,29 @@
 let username = document.querySelector("#username")
-let password =document.querySelector("#pwd")
+let password = document.querySelector("#pwd")
 
 let getUsername = localStorage.getItem("username")
-let getpassword =localStorage.getItem("password")
-let loginBtn= document.querySelector("#logbtn")
+let getPassword = localStorage.getItem("password")
 
-loginBtn.addEventListener("click",function(e){
+let loginBtn = document.querySelector("#logbtn")
+
+loginBtn.addEventListener("click", function(e) {
     e.preventDefault()
-    if(username.value===""||password.value===""){
-        alert("please enter missed data ")
-    }else{
-        if(getUsername&&getUsername.trim()===username.value &&getpassword&&getpassword.trim()===password.value){
-            setTimeout(()=>{
-                window.location="index.html"
-            } ,1500)
-   
 
-        }else{
-            alert("username or password is incorrect , please try again ")
+    if (username.value.trim() === "" || password.value.trim() === "") {
+        alert("Please enter missing data")
+    } else {
+        if (
+            getUsername &&
+            getPassword &&
+            getUsername.trim().toLowerCase() === username.value.trim().toLowerCase() &&
+            getPassword.trim() === password.value.trim()
+        ) {
+            alert("Login successful!")
+            setTimeout(() => {
+                window.location = "index.html"
+            }, 1000)
+        } else {
+            alert("Username or password is incorrect, please try again")
         }
     }
 })
-
